6be1c67b3178924f3d4c394a5a9d9c778863dc21
mounikakompalli <kompallimounika@gmail.com>
2016-07-22 12:12:34 -0400

    adding all files

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module("LocationFinder.controllers",[]).controller("LocationSearchController",['$scope','$location',function($scope,$location){
+angular.module("LocationFinder.controllers",['LocationFinder.services']).controller("LocationSearchController",['$scope','$location','LocationFinderAppService',function($scope,$location,LocationFinderAppService){
 
 	
 
