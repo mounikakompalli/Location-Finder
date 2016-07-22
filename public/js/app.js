04c8410d61f4b1130177ef469603c0cb17589803
bharath559 <kbharath00@gmail.com>
2016-07-22 12:44:07 -0400

    adding services

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
 var locationSearchApp = angular.module("LocationFinder", [
+	"LocationFinder.services",
+    "LocationFinder.controllers",
+    "ngRoute"
 ]);
 locationSearchApp.config(function($routeProvider) {
     $routeProvider
