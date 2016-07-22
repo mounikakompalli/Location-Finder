1d5122c485a640ba0e3c62227382f3f8c7f1eb69
bharath559 <kbharath00@gmail.com>
2016-07-22 12:52:37 -0400

    adding services

--- public/js/services.js
+++ public/js/services.js
@@ -4,7 +4,7 @@ angular.module("LocationFinder.services",[]).service("LocationFinderAppService",
 	this.getSearchData = function(lat,long){
 
 		var googleAPIKey = "AIzaSyCqykw3PCrky_M5n6YQ8qZhGiFlTiHy40c";
-		var googleAPIUrl = "http://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
+		var googleAPIUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
 		googleAPIUrl+=lat+","+long+"&radius=500&key="+googleAPIKey;
 
 		return $http.
